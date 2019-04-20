e239b89eb81151b56557440b6283e8420703baaf
irustm <jamakrus@gmail.com>
2019-04-20 18:03:56 +0300

    fix #2

--- src/replace-pipes.ts
+++ src/replace-pipes.ts
@@ -1,6 +1,6 @@
 import { ElementAst } from '@angular/compiler';
 import { DirectiveSymbol } from 'ngast';
-import { getPipeAst, PipeSourceAst } from './utils/pipe-ast';
+import { getPipeAst, PipeBoundText } from './utils/pipe-ast';
 import { readFileSync, writeFileSync } from 'fs';
 import { CliConfig } from './models/models';
 
@@ -10,25 +10,33 @@ export function replacePipes(allDirectives: DirectiveSymbol[], config: CliConfig
     let url = null;
     try {
       if (el.isComponent()) {
-        let translatePipes: PipeSourceAst[] = [];
+        let translatePipes: PipeBoundText[] = [];
         url = el.getResolvedMetadata().templateUrl || el.symbol.filePath;
-        // console.log(el.getResolvedMetadata().styleUrls);
         el.getTemplateAst().templateAst.forEach((element, i) => {
           translatePipes.push(...getPipeAst(element as ElementAst, 'translate'));
         });
 
         let replaces: { from: string; to: string }[] = translatePipes.map((pipe, i) => {
-          const text = getParamWithString(translates, pipe.value)
-          if(text){
+          // get all pipes in one textBound source
+          let replaceResult = pipe.source;
+          pipe.pipeValues.forEach(pipeValue => {
+            const text = getParamWithString(translates, pipeValue.value);
+            if(text){
+              const replace = `{{[\\s*]?'${pipeValue.value}'[\\s+]?\\|?\\s?\\w*\\s?}}`;
+              replaceResult = replaceResult.replace(new RegExp(replace, 'g'), text.trim());
+            } else {
+              console.warn(`translate for pipe: ${pipeValue.value} not found`);
+            }
+          });
+          if(replaceResult !== pipe.source){
+            const translateVars = `${pipe.pipeValues.map(el => el.value).join(',')}`;
             return {
               from: `>${pipe.source}`,
-              to: ` i18n>${text.trim()}`
+              to: ` i18n="${translateVars}">${replaceResult}`
             };
           } else {
-            console.warn(`translate for pipe: ${pipe.value} not found`);
             return null;
           }
-          
         });
         let sourceCode = readFileSync(url).toString();
         replaces = replaces.filter(Boolean);
@@ -43,7 +51,7 @@ export function replacePipes(allDirectives: DirectiveSymbol[], config: CliConfig
     } catch (e) {
       // Component
       // exception only componentß
-      console.log(url);
+      // console.log(url);
       console.error(e);
     }
   });
--- src/utils/pipe-ast.ts
+++ src/utils/pipe-ast.ts
@@ -1,29 +1,43 @@
-import { ElementAst } from '@angular/compiler';
-export interface PipeSourceAst {
+import { ElementAst, Interpolation } from '@angular/compiler';
+export interface PipeBoundText {
+  ast: Interpolation;
+  parentNode: ElementAst;
   source: string;
+  pipeValues: PipeSourceAst[];
+}
+export interface PipeSourceAst {
+  expression: any;
   value: string;
 }
-export function getPipeAst(element: ElementAst, pipeName = 'translate'): PipeSourceAst[] {
-  const pipes: PipeSourceAst[] = [];
+export function getPipeAst(element: ElementAst, pipeName = 'translate'): PipeBoundText[] {
+  const pipeBounds: PipeBoundText[] = [];
   if (element && element.children && element.children.length) {
     element.children.forEach((child: any) => {
       const name = child.constructor.name;
       const value: any = child.value;
       if (value) {
         if (name === 'BoundTextAst') {
-          // only first expression check
-          const expression = value.ast.expressions[0];
-          if (expression && expression.name === pipeName) {
-            pipes.push({
+          const expressions = value.ast.expressions.filter(expression => expression.name === pipeName);
+          if (expressions && expressions.length > 0) {
+            const pipesValues: PipeSourceAst[] = [];
+            expressions.forEach(expression => {
+              pipesValues.push({
+                expression,
+                value: expression.exp.value,
+              });
+            });
+            pipeBounds.push({
+              ast: value.ast,
+              parentNode: element,
               source: value.source,
-              value: expression.exp.value
+              pipeValues: pipesValues
             });
           }
         }
       } else {
-        return pipes.push(...getPipeAst(child as ElementAst));
+        return pipeBounds.push(...getPipeAst(child as ElementAst));
       }
     });
   }
-  return pipes;
+  return pipeBounds;
 }
